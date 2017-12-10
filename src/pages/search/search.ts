690767ab888868c1e4c80070c9f9a144a1ddfd3e
Samiullah Khan <samiullah.khan.1989@gmail.com>
2017-12-10 23:35:18 +0500

    improved list style, ordered by date

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -11,17 +11,27 @@ import { categories } from "../../shared/categories";
   templateUrl: 'search.html',
 })
 export class SearchPage {
+  
+  categories: any = [];
+  category;
 
+  searchDate;
   expRef: AngularFirestoreCollection<any>;
   expenses$: Observable<Expense[]>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public afs: AngularFirestore) {
+    Object.assign(this.categories, categories);
   }
 
   ionViewDidLoad() {
-    this.expRef = this.afs.collection('expense', ref => ref.where('category','==','Food'));    
+  }
+  
+  public loadResults(){
+    console.log('Loading Data');
+    
+    this.expRef = this.afs.collection('expense', ref => ref.where('category','==', this.category));     
     this.expenses$ = this.expRef.valueChanges();
-    console.log('ionViewDidLoad SearchPage');
+
   }
 
 }
