edb1a7aa36a62919e9632583e279d6b8c83b2e30
Samiullah <samiullah.khan.1989@gmail.com>
2018-06-01 15:11:20 +0400

    added search switcher of range and month

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -1,43 +1,57 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { AngularFirestoreCollection, AngularFirestore } from 'angularfire2/firestore';
+import {
+  AngularFirestoreCollection,
+  AngularFirestore
+} from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
 import { Expense } from '../home/expense.model';
-import { categories } from "../../shared/categories";
+import { categories } from '../../shared/categories';
 
 @IonicPage({})
 @Component({
   selector: 'page-search',
-  templateUrl: 'search.html',
+  templateUrl: 'search.html'
 })
 export class SearchPage {
-
-  loading:boolean = false;
+  loading: boolean = false;
   categories: any = [];
-  category;
+  searchType: string = 'month';
+  filter = {
+    startDate: '',
+    endDate: '',
+    category: '',
+    month: ''
+  };
 
-  searchDate;
   expRef: AngularFirestoreCollection<any>;
   expenses$: Observable<Expense[]>;
 
   total: number = 0;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public afs: AngularFirestore) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public afs: AngularFirestore
+  ) {
     Object.assign(this.categories, categories);
   }
 
-  ionViewDidLoad() {
-  }
+  ionViewDidLoad() {}
 
   public loadResults() {
-    this.loading = true;
-    this.expRef = this.afs.collection('expense', ref => ref.where('category', '==', this.category));
-    this.expenses$ = this.expRef.valueChanges();
+    console.log(this.filter);
     
+    this.loading = true;
+    this.expRef = this.afs.collection('expense', ref =>
+      ref.where('category', '==', this.filter.category)
+    );
+    this.expenses$ = this.expRef.valueChanges();
+
     this.expenses$.forEach(values => {
-      this.total = values.reduce((prev, current, ) => {
+      this.total = values.reduce((prev, current) => {
         return prev + Number(current.price);
-      }, 0)
+      }, 0);
     });
     this.loading = false;
   }
