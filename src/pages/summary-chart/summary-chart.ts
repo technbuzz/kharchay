e4c3ad8f8de41a6a3037a08be0241e0d5db66c19
Samiullah Khan <samiullah.khan.1989@gmail.com>
2018-06-04 00:44:44 +0400

    adding real data

--- src/pages/summary-chart/summary-chart.ts
+++ src/pages/summary-chart/summary-chart.ts
@@ -1,41 +1,72 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the SummaryChartPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { AngularFirestoreCollection, AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import { Expense } from '../home/expense.model';
+import { startOfMonth, endOfMonth } from 'date-fns';
 
 @IonicPage()
 @Component({
   selector: 'page-summary-chart',
-  templateUrl: 'summary-chart.html',
+  templateUrl: 'summary-chart.html'
 })
 export class SummaryChartPage {
-  public doughnutChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
-  public doughnutChartData:number[] = [350, 450, 100];
-  public doughnutChartType:string = 'doughnut';
-  donutOptions: any = {
-    options:{
-      legend:{position:'right'}
-    }
-  }
+  loading: boolean = true;
+  expRef: AngularFirestoreCollection<any>;
+  expenses$: Observable<Expense[]>
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public doughnutChartLabels: string[] = [
+    'Download Sales',
+    'In-Store Sales',
+    'Mail-Order Sales'
+  ];
+  public doughnutChartData: number[] = [350, 450, 100];
+  public doughnutChartType: string = 'doughnut';
+  donutOptions: any = {
+    legend: {
+      display: true,
+      position: 'right',
+      labels: {
+        // fontColor: 'rgb(255, 99, 132)'
+      }
+    }
+  };
+
+  total:number = 0;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private afs: AngularFirestore) {
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SummaryChartPage');
+    this.getCurrentMonthStats();
   }
 
-  chartHovered(){
+  getCurrentMonthStats(){
+    const basicStartMonth = startOfMonth(new Date());
+    const basicEndMonth = endOfMonth(new Date());
+
+    this.loading = true;
+    this.expRef = this.afs.collection('expense', ref =>
+      ref
+        .where('date', '>=', basicStartMonth)
+        .where('date', '<=', basicEndMonth)
+    );
+
+    // Finding Total
+    this.expenses$ = this.expRef.valueChanges();
+    this.expenses$.forEach(values => {
+      this.total = values.reduce((prev, current) => {
+        return prev + Number(current.price);
+      }, 0);
+      console.log(values);
+      
+    });
 
   }
 
-  chartClicked(){
+  chartHovered() {}
 
-  }
-
+  chartClicked() {}
 }
