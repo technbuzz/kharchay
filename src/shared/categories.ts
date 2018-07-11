4b7faa1bd12256933d68a303da16c414e8a180fd
Samiullah Khan <samiullah.khan.1989@gmail.com>
2018-07-11 23:14:36 +0400

    added other food type

--- src/shared/categories.ts
+++ src/shared/categories.ts
@@ -2,7 +2,7 @@ export const categories = [
   { title:'bills'},
   { title:'clothes'},
   { title:'entertainment'},
-  { title:'food', subCategory: ['breakfast','lunch','dinner']},
+  { title:'food', subCategory: ['breakfast','lunch','dinner','other']},
   { title:'health'},
   { title:'snacks'},
   { title:'transport'},
