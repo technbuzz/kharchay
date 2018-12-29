f17c73e4121d288252d7105acaed0cf6b2129acd
Samiullah Khan <samiullah.khan.1989@gmail.com>
2018-12-29 15:01:42 +0400

    making it even better

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyCUzUayLoSbLiUQUSe4Q4t8H4hOENZTJMc',
+    authDomain: 'kharchay-95809.firebaseapp.com',
+    databaseURL: 'https://kharchay-95809.firebaseio.com',
+    projectId: 'kharchay-95809',
+    storageBucket: 'kharchay-95809.appspot.com',
+    messagingSenderId: '830324159049'
+  }
+};
