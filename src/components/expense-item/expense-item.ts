3395a1332a136706555fcf4d708f1be66955bad7
Samiullah Khan <samiullah.khan.1989@gmail.com>
2018-12-29 12:35:17 +0400

    added support for  pwa and heroku

--- src/components/expense-item/expense-item.ts
+++ src/components/expense-item/expense-item.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { IExpense } from '../../shared/expense.interface';
 import { NavController } from 'ionic-angular';
 
